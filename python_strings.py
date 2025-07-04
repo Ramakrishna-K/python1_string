79bf085e340fc12acb08ca549d640c4b1f9327cb
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-04 22:22:57 +0530

    string in pyhton

---
+++ python_strings.py
@@ -0,0 +1,111 @@
+# In Python, strings are sequences of characters enclosed in single quotes ('), double quotes ("), or triple quotes (''' / """).
+
+# # 1 creating string in python
+
+# text = "Ramakrishna"
+# a = 'hello '
+# b = '''this is a multi-line string'''
+
+# print(text)
+# print(a)
+# print(b)
+
+# # 2 Accessing Characters
+# # Strings are indexed — starting from 0.
+# name = "Ramakrishna"
+# print(name[0])
+# print(name[1])
+# print(name)
+# using negative indexing to access from the end
+# print(name[-1]) 
+
+# # 3 string slicing(
+
+# text = "Rama krishna"
+
+# print(text[:4])
+# print(text[5:])
+
+# 4 string methods
+
+# # 4.1 uppercase
+# name = 'ramakrishna'
+# print(name.upper())
+
+# # 4.2 lowercase
+# name = "KISHAN"
+# print(name.lower())
+
+# 4.3 strip
+# name =  "ramakrishna"
+# print(name.strip())
+
+# 4.4 replace 
+
+# name = "ramakrishna"
+# print(name.replace("rama",'Ram'))
+
+# # 4.5 split 
+# name = "Ramakrishna"
+# print(name.split())
+
+# # 5 string concatenation and Repetition
+# a = 'hello'
+# b =  ' python'
+# c = a + b
+
+# print(c)
+
+# #  6 String Formatting
+# name = "Ram"
+# age = 24
+# print("My name is {} and i am {} years old.".format(name,age))
+
+# # 7 check string content
+
+# name = "krihsna"
+# print(name.isalpha())   
+# print(name.isdigit())   
+# print(name.isalnum()) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
